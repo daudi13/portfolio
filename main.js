5cc7bccfaf8c59628a72f9379801e6e6778ed6c4
daudi13 <davouma13@gmail.com>
2022-01-24 18:07:42 +0300

    Add updated linters

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-const btn = document.querySelector('.header__menu-btn')
-const mobileMenu = document.querySelector('.mobile-menu')
-btn.addEventListener('click', function(){
-     mobileMenu.classList.toggle('active')
-     btn.classList.toggle('active')
-})
+const mobileMenu = document.querySelector('.mobile-menu');
+const menuLinks = document.querySelectorAll('.nav-mobile');
+const btn = document.querySelector('.header__menu-btn');
+
+btn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');
+  btn.classList.toggle('active');
+});
+
+menuLinks.forEach((link) => link.addEventListener('click', () => {
+  mobileMenu.classList.remove('active');
+  btn.classList.remove('active');
+}));
