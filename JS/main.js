8e665c63a1114bec2206deac344d2a96fc87b096
daudi13 <davouma13@gmail.com>
2022-01-28 07:12:44 +0300

    Add local storage

--- JS/main.js
+++ JS/main.js
@@ -18,13 +18,41 @@ menuLinks.forEach((link) => link.addEventListener('click', () => {
 
 const userEmail = document.getElementById('email');
 const dialog = document.querySelector('.callAction');
-const contactForm = document.querySelector('form');
+const form = document.querySelector('form');
 
-contactForm.addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
   const emailInput = userEmail.value;
 
   if (/[A-Z]/.test(emailInput)) {
     dialog.innerHTML = '*Please ensure your email address is in lowercase';
     e.preventDefault();
   }
+});
+
+// local storage
+
+const clientName = document.getElementById('userName');
+const clientEmail = document.getElementById('userEmail');
+const clientMessage = document.getElementById('message');
+const formKey = 'form';
+
+window.onload = () => {
+  const formData = localStorage.getItem(formKey);
+
+  if (formData) {
+    const form = JSON.parse(formData);
+    clientName.value = form.clientName;
+    clientEmail.value = form.clientEmail;
+    clientMessage.value = form.clientMessage;
+  }
+};
+
+form.addEventListener('input', () => {
+  const formObj = {
+    clientName: clientName.value,
+    clientEmail: clientEmail.value,
+    clientMessage: clientMessage.value,
+  };
+
+  localStorage.setItem(formKey, JSON.stringify(formObj));
 });
